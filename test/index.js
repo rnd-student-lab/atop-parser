2cb1db904d981f3bbac7c47549251a15ecfa4437
Dmitry Ilin <i@dmitryilin.com>
2022-09-20 16:26:11 +0300

    Add tests for one of the library functions

--- test/index.js
+++ test/index.js
@@ -1,9 +1,51 @@
 import { expect } from 'chai';
-// import { atopLogsToObject, atopLogsToObjectStream } from '../src';
+import typeDetect from 'type-detect';
+import { getAtopFields } from '../src';
 
-describe('Atop parser test suite.', () => {
-  it('should work', () => {
-    // TBD
-    expect(true).to.be.deep.equal(true);
+// TBD
+
+describe('Check getAtopFields function.', () => {
+  const properties = [
+    'CPU',
+    'cpu',
+    'CPL',
+    'MEM',
+    'SWP',
+    'PAG',
+    'LVM',
+    'MDD',
+    'DSK',
+    'NET',
+    'PRG',
+    'PRC',
+    'PRM',
+    'PRD',
+    'PRN',
+  ];
+
+  it('should be an object', () => {
+    const fields = getAtopFields();
+    expect(fields).to.be.an('Object');
+  });
+
+  it('should describe all atop labels', () => {
+    const fields = getAtopFields();
+    expect(fields).to.have.all.keys(properties);
+  });
+
+  it('every label must contain an array of descriptions', () => {
+    const fields = getAtopFields();
+    for (let i = 0; i < properties.length; i += 1) {
+      expect(fields[properties[i]]).to.be.an('Array');
+    }
+  });
+
+  it('every array of descriptions must contain strings or arrays', () => {
+    const fields = getAtopFields();
+    for (let i = 0; i < properties.length; i += 1) {
+      for (let j = 0; j < fields[properties[i]].length; j += 1) {
+        expect(typeDetect(fields[properties[i]][j])).to.be.oneOf(['string', 'Array']);
+      }
+    }
   });
 });
